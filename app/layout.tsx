1f64a8a4e614737761a192cb371da8acd3019a69
Avaz Bokiev <html5web@gmail.com>
2024-10-27 15:08:53 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'AI Friend',
+  title: 'GPT',
   description: 'AI Personal Assistant App',
 };
 
